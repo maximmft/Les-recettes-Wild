9dcd96a9261438afbd9545950cc2528193309aea
maximmft <mmaufront@gmail.com>
2024-03-15 17:08:52 +0100

    fixed bug

---
+++ javascript/accueil.js
@@ -0,0 +1,17 @@
+
+// Slider in welcome page
+
+const sliderNavButtonPrevious = document.querySelector(".slider-nav-button--previous");
+const sliderNavButtonNext = document.querySelector(".slider-nav-button--next");
+
+
+
+sliderNavButtonPrevious.addEventListener("click", function(){
+  const widthSlider = document.querySelector(".slider-content").offsetWidth;
+  document.querySelector(".slider-content").scrollLeft -= widthSlider;
+});
+
+sliderNavButtonNext.addEventListener("click", function(){
+  const widthSlider = document.querySelector(".slider-content").offsetWidth;
+    document.querySelector(".slider-content").scrollLeft += widthSlider;
+});
