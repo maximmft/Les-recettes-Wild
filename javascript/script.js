0833cd05126cb2286bb63ea1bccdd6bc396a2529
Aude Sedillo <aude.sedillo@gmail.com>
2024-03-15 16:51:24 +0100

    modified script js section comment

--- javascript/script.js
+++ javascript/script.js
@@ -8,13 +8,14 @@ closeBtn.onclick = closeNav;
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  sideNav.classList.add("active");
-  overlay.style.display = "block";
+    sideNav.classList.add("active");
+    overlay.style.display = "block";
 
+}
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  sideNav.classList.remove("active");
-  overlay.style.display = "none";
+    sideNav.classList.remove("active");
+    overlay.style.display = "none";
 }
 
 
@@ -39,18 +40,18 @@ if (document.body.clientWidth > 900) {
         function sticky() {
             let posCurseur = this.pageYOffset;
 
-      if (memoPositionBar - posCurseur < 1) {
-          
-          stickySideBar.style.position = "fixed";
-          stickySideBar.style.top = "0px";
-          stickySideBar.style.zIndex = 3;
-          stickySideBar.style.height = "100%";
-          stickySideBar.style.padding = "30px";
-          stickySideBar.style.paddingTop = "10%";
-          stickySideBar.style.width = "20%";
-          recetteSticky.style.width = "75%";
-          recetteSticky.style.marginLeft = "35%";
-      }
+            if (memoPositionBar - posCurseur < 1) {
+
+                stickySideBar.style.position = "fixed";
+                stickySideBar.style.top = "0px";
+                stickySideBar.style.zIndex = 3;
+                stickySideBar.style.height = "100%";
+                stickySideBar.style.padding = "30px";
+                stickySideBar.style.paddingTop = "10%";
+                stickySideBar.style.width = "20%";
+                recetteSticky.style.width = "75%";
+                recetteSticky.style.marginLeft = "35%";
+            }
 
 
             if (posCurseur < 366) {
@@ -80,11 +81,11 @@ const list = document.querySelector("#list");
 const commentPost = document.querySelector(".comment-post");
 
 commentButton.addEventListener('click', () => {
-    if (newComment.value){
-    const clone = comment.cloneNode(true);
-    clone.children[1].innerText = newComment.value;
-    list.insertBefore(clone, newComment);
-    newComment.value = "";
+    if (newComment.value) {
+        const clone = comment.cloneNode(true);
+        clone.children[1].innerText = newComment.value;
+        list.insertBefore(clone, newComment);
+        newComment.value = "";
     }
 })
 
