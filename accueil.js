698686a54bbd0aed3af4a7cccec73875b78609f2
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-03-15 10:53:35 +0100

    creation slider

--- accueil.js
+++ accueil.js
@@ -7,11 +7,15 @@ const sliderNavButtonNext = document.querySelector(".slider-nav-button--next");
 
 
 sliderNavButtonPrevious.addEventListener("click", function(){
-  // const widthSlider = document.querySelector(".slider-content").offsetWidth;
-  document.querySelector(".slider-content").scrollLeft -= 100;
+  const widthSlider = document.querySelector(".slider-content").offsetWidth;
+  document.querySelector(".slider-content").scrollLeft -= widthSlider;
 });
 
 sliderNavButtonNext.addEventListener("click", function(){
-  // const widthSlider = document.querySelector(".slider-content").offsetWidth;
-  document.querySelector(".slider-content").scrollLeft += 100;
+  const widthSlider = document.querySelector(".slider-content").offsetWidth;
+  
+  // si dernière image : revenir au début.
+  // sinon, continnuer à slider sur la gauche.
+    document.querySelector(".slider-content").scrollLeft += widthSlider;
 });
+
