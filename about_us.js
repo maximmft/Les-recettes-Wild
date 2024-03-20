a37ef4f2159896ec45def1fd04a55d3186155804
maximmft <mmaufront@gmail.com>
2024-03-20 14:05:37 +0100

    final version without Aude part

--- about_us.js
+++ about_us.js
@@ -4,3 +4,14 @@ const more = document.querySelector("#more");
 more.addEventListener("click", ()=>{
     readMoreText.classList.toggle("visible");
 })
+
+const button = document.querySelector("#bouton")
+const textArea= document.querySelector("textarea")
+const message = "Merci de nous avoir contacté"
+const errorMessage = "Veuiller insérer votre texte"
+
+button.addEventListener("click", function(event){
+    event.preventDefault
+    if(!textArea.value) {alert (errorMessage)}
+    else {alert(message)}
+})
